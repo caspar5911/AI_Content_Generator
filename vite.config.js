e68ce0553047f4593ee66d6e3698a1d0a2092d9f
caspar5911 <caspar95galaxy@gmail.com>
2024-12-22 13:51:10 +0800

    fix: vite config base for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AI_CONTENT_GENERATOR/', // Update this with your repository name
   plugins: [react()],
 })
