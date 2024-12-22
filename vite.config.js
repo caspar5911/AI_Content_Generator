e6e541b9d87ecc72038c2c7d51b0399918f201b6
caspar5911 <caspar95galaxy@gmail.com>
2024-12-22 13:53:56 +0800

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AI_CONTENT_GENERATOR/', // Update this with your repository name
+  base: '/AI_Content_Generator/', // Update this with your repository name
   plugins: [react()],
 })
